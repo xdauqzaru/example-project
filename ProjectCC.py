1a61b5dfebd85b5c8cd3ebd0f8d74f0bf0c3ec6a
Daniel Arteni <daniel.arteni@student.tuiasi.ro>
2021-05-30 21:01:49 +0300

    add store

--- ProjectCC.py
+++ ProjectCC.py
@@ -77,7 +77,7 @@ def Register():
             cursor.execute(insert_sql, ('', first_name, last_name, address, phone, email, password))
             db_conn.commit()
 
-            image_file_name_in_s3 = "user_" + last_name + "_" + first_name + "_image_file"
+            image_file_name_in_s3 = "user_" + last_name + "_" + first_name + "_image_file.jpg"
             s3 = boto3.resource('s3')
 
             try:
@@ -104,7 +104,7 @@ def Register():
     else:
         return render_template('register.html', text="This email is already in our database.")
 
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route("/login", methods=['POST', 'GET'])
 def Login():
@@ -118,7 +118,38 @@ def Login():
     if out == None:
         return render_template('login.html', text="Email or password is wrong. Please try again!")
     else:
-        return redirect('index.html')
+        return redirect('store.html')
+
+@app.route("/contact", methods=['POST'])
+def Contact(req, res):
+    email = req.body.email   
+    subject = req.body.subject
+
+    file_object = open('contact.txt', 'a')
+    file_object.write("Email: " + email + "\nSubject: " + subject + "\n\n")
+    file_object.close()
+    # s3 = boto3.resource('s3')
+    # try:
+    #     s3.Bucket(custombucket).put_object(Key=image_file_name_in_s3, Body=image)
+    #     bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+    #     s3_location = (bucket_location['LocationConstraint'])
+
+    #     if s3_location is None:
+    #         s3_location = ''
+    #     else:
+    #         s3_location = '-' + s3_location
+
+    #     object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+    #                 s3_location,
+    #                 custombucket,
+    #                 image_file_name_in_s3)
+
+    # except Exception as e:
+    #     return str(e)
+
+    rez = "<div><p>Message sent!</p></div>"
+    res.send(rez)
+    res.end()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
